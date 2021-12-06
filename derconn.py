b3f27ed16023ead03327e17485ad9f48701cb355
Sterph <stephan.palm98@gmail.com>
2021-12-06 16:23:51 -0500

    updated time series extraction function

--- derconn.py
+++ derconn.py
@@ -1,16 +1,23 @@
 import numpy as np
 import nibabel as nib
-
+from nilearn import image, input_data
+from nimlab import datasets as nimds
 
 def test_function():
     print("This is a test!")
 
 def extract_ts(roi_path, rs_path):
-    roi_msk_img = nib.load(roi_path)
-    roi_msk_dat = roi_msk_img.get_fdata()
-    rs = np.load('rs_path')
+    roi_img = nib.load(roi_path)
+    roi_dat = roi_img.get_fdata()
+    rs = np.load(rs_path)
 
-    roi_msk_dat[roi_msk_dat != 0] = 1
-    rs[roi_msk_dat]
+    MNI_brain_mask = nimds.get_img("MNI152_T1_2mm_brain_mask")
+    brain_masker = input_data.NiftiMasker(MNI_brain_mask)
+    roi_msk = brain_masker.fit_transform(roi_img)
+    roi_msk = np.repeat(roi_msk, rs.shape[0], axis = 0)
 
-    nilearn niftimasker.transform()
+    rs_mskd = rs * roi_msk
+    roi_ts = np.mean(rs_mskd, 1)
+    vox_tss = rs
+
+    return roi_ts, vox_tss
