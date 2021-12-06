a73372577d712c9393d405c5e85a4bdbb8e5d134
Christopher Lin <clin@bwh.harvard.edu>
2021-12-06 15:05:18 -0500

    Finish fit_curve

--- derconn.py
+++ derconn.py
@@ -1,10 +1,15 @@
 import numpy as np
 from scipy import optimize
+from scipy import misc
 
 def test_function():
     print("This is a test!")
 
 def fit_curve(roi_ts, vox_tss, f):
     roi_curve = f(np.arange(0,len(roi_ts)),roi_ts)
-    print(roi_curve)
-    return
+    vox_curves = []
+    for v in vox_tss:
+        vox_curve = f(np.arange(0,len(v),v))
+        vox_curves.append(v)
+
+    return roi_curve, vox_curves
