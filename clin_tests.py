a63535b48e0e233614e2312548cdf2a494ed44ec
Christopher Lin <clin@bwh.harvard.edu>
2021-12-06 15:07:56 -0500

    Fix parenthases

--- clin_tests.py
+++ clin_tests.py
@@ -7,7 +7,9 @@ def test_fit_curve():
     roi_ts = np.random.rand(240)
     vox_tss = [np.random.rand(240) for x in range(5)]
     func = interpolate.UnivariateSpline
-    dc.fit_curve(roi_ts, vox_tss, func)
+    result = dc.fit_curve(roi_ts, vox_tss, func)
+
+
 
 
 if __name__=="__main__":
--- derconn.py
+++ derconn.py
@@ -9,7 +9,7 @@ def fit_curve(roi_ts, vox_tss, f):
     roi_curve = f(np.arange(0,len(roi_ts)),roi_ts)
     vox_curves = []
     for v in vox_tss:
-        vox_curve = f(np.arange(0,len(v),v))
+        vox_curve = f(np.arange(0,len(v)),v)
         vox_curves.append(v)
 
     return roi_curve, vox_curves
