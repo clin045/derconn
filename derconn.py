da78006490f3f98238d789ec4f8cf6cfc37c7ce3
Sterph <stephan.palm98@gmail.com>
2021-12-06 16:19:33 -0500

    added time series extraction function

--- derconn.py
+++ derconn.py
@@ -1,2 +1,16 @@
+import numpy as np
+import nibabel as nib
+
+
 def test_function():
     print("This is a test!")
+
+def extract_ts(roi_path, rs_path):
+    roi_msk_img = nib.load(roi_path)
+    roi_msk_dat = roi_msk_img.get_fdata()
+    rs = np.load('rs_path')
+
+    roi_msk_dat[roi_msk_dat != 0] = 1
+    rs[roi_msk_dat]
+
+    nilearn niftimasker.transform()
